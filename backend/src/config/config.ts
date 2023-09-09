f31d39d9e4f1f13d597aa89af31c4ec3b3af9b30
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-09-09 23:47:44 +0300

    docker

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,9 +1,5 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 export const config = {
-    PORT: process.env.PORT || 7500,
+    PORT: process.env.PORT,
 
     MYSQL_DATABASE_NAME: process.env.MYSQL_DATABASE_NAME,
 
